8ca8453b91bf13ffd376670cf64983095ecc1b3d
Hector Enrique Gomez Morales <hectoregm@gmail.com>
2013-06-15 15:27:55 -0500

    Initial Commit

---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Hector Enrique Gomez Morales on 6/15/13.
+//  Copyright (c) 2013 Hector Enrique Gomez Morales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
