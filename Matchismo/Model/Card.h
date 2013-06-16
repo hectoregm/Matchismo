62d46e6b36ccef41097659bceace8a8104c734f2
Hector Enrique Gomez Morales <hectoregm@gmail.com>
2013-06-16 08:22:00 -0500

    Add Card model.

---
+++ Matchismo/Model/Card.h
@@ -0,0 +1,13 @@
+//
+//  Card.h
+//  Matchismo
+//
+//  Created by Hector Enrique Gomez Morales on 6/15/13.
+//  Copyright (c) 2013 Hector Enrique Gomez Morales. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+@end
---
+++ Matchismo/Model/Card.m
@@ -0,0 +1,13 @@
+//
+//  Card.m
+//  Matchismo
+//
+//  Created by Hector Enrique Gomez Morales on 6/15/13.
+//  Copyright (c) 2013 Hector Enrique Gomez Morales. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+@end
