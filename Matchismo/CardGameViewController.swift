0e3d0f865134f04b6bbc02edee868ce3c237d6ff
Hector E. Gomez Morales <hectoregm@gmail.com>
2014-06-17 19:48:31 -0500

    Renamed main view controller.

--- Matchismo/CardGameViewController.swift
+++ Matchismo/CardGameViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CardGameViewController.swift
 //  Matchismo
 //
 //  Created by Hector Enrique Gomez Morales on 6/17/14.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CardGameViewController: UIViewController {
                             
     override func viewDidLoad() {
         super.viewDidLoad()
